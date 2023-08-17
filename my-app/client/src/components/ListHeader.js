d3c198a4c9696a145fe207966b95bc4345b1d452
yuktea <73362292+yuktea@users.noreply.github.com>
2023-08-17 06:52:14 +0530

    feat: add sign-out button

--- my-app/client/src/components/ListHeader.js
+++ my-app/client/src/components/ListHeader.js
@@ -3,7 +3,14 @@ import { useState } from 'react'
 import { useCookies } from 'react-cookie'
 
 const ListHeader = ({ listName, getData }) => {
+  const [cookies, setCookie, removeCookie] = useCookies(null)
+  const [showModal, setShowModal] = useState(false)
 
+  const signOut = () => {
+    console.log('signout')
+    removeCookie('Email')
+    removeCookie('AuthToken')
+    window.location.reload()
 
   }
 
@@ -12,6 +19,7 @@ const ListHeader = ({ listName, getData }) => {
       <h1>{listName}</h1>
       <div className="button-container">
         <button className="create" onClick={() => setShowModal(true)}>ADD NEW</button>
+        <button className="signout" onClick={signOut}>SIGN OUT</button>
       </div>
       {showModal && <Modal mode={'create'} setShowModal={setShowModal} getData={getData} />}
     </div>
