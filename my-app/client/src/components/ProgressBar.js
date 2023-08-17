7fdfbf111f05b37b90ce12dc7385302e8f7c02cd
yuktea <73362292+yuktea@users.noreply.github.com>
2023-08-17 07:56:57 +0530

    style: random colors for progress bar

--- my-app/client/src/components/ProgressBar.js
+++ my-app/client/src/components/ProgressBar.js
@@ -6,11 +6,13 @@ const ProgressBar = ({ progress }) => {
     'rgb(141, 181, 145)'
   ]
 
+  const randomColor = colors[Math.floor(Math.random() * colors.length)]
 
   return (
     <div className="outer-bar">
       <div
         className="inner-bar"
+        style={{ width: `${progress}%`, backgroundColor: randomColor }}
       ></div>
     </div>
   )
