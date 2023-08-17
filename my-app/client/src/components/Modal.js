7cec778994f9d8187c91abc500d96972169ae936
yuktea <73362292+yuktea@users.noreply.github.com>
2023-08-17 18:03:32 +0530

    feat: component for modals

--- my-app/client/src/components/Modal.js
+++ my-app/client/src/components/Modal.js
@@ -1,2 +1,40 @@
 import { useState } from 'react'
 import { useCookies } from 'react-cookie'
+  return (
+    <div className="overlay">
+      <div className="modal">
+        <div className="form-title-container">
+          <h3>Let's {mode} your task</h3>
+          <button onClick={() => setShowModal(false)}>X</button>
+        </div>
+
+        <form>
+          <input
+            required
+            maxLength={30}
+            placeholder=" Your task goes here"
+            name="title"
+            value={data.title}
+            onChange={handleChange}
+          />
+          <br />
+          <label for="range">Drag to select your current progress</label>
+          <input
+            required
+            type="range"
+            id="range"
+            min="0"
+            max="100"
+            name="progress"
+            value={data.progress}
+            onChange={handleChange}
+          />
+          <input className={mode} type="submit" onClick={editMode ? editData: postData} />
+        </form>
+
+      </div>
+    </div>
+  )
+}
+
+export default Modal
