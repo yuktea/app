51a5c2a3a20b16d47e64a2d3a7425fc0540d1c1b
yuktea <73362292+yuktea@users.noreply.github.com>
2023-08-21 15:23:04 +0530

    add post and edit handlers for modals

--- my-app/client/src/components/Modal.js
+++ my-app/client/src/components/Modal.js
@@ -1,5 +1,56 @@
 import { useState } from 'react'
 import { useCookies } from 'react-cookie'
+
+const Modal = ({ mode, setShowModal, getData, task }) => {
+  const [cookies, setCookie, removeCookie] = useCookies(null)
+  const editMode = mode === 'edit' ? true : false
+
+  const [data, setData] = useState({
+    user_email: editMode ? task.user_email : cookies.Email,
+    title: editMode ? task.title : null,
+    progress: editMode ? task.progress : 50,
+    date: editMode ? task.date : new Date()
+  })
+
+  const postData = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos`, {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+      })
+      if (response.status === 200) {
+        console.log('WORKED')
+        setShowModal(false)
+        getData()
+      }
+
+    } catch(err) {
+      console.error(err)
+    }
+  }
+
+
+  const editData = async(e) => {
+    e.preventDefault()
+    try {
+      const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos/${task.id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+      })
+      if (response.status === 200) {
+        setShowModal(false)
+        getData()
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  
+
   return (
     <div className="overlay">
       <div className="modal">
