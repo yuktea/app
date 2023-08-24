3fd9615d905d983f5efa2ededd1c6eb59f88f183
yuktea <73362292+yuktea@users.noreply.github.com>
2023-08-24 11:34:14 +0530

    SQL for users table

--- my-app/server/data.sql
+++ my-app/server/data.sql
@@ -1,8 +1,14 @@
 CREATE DATABASE my-app;
+
 CREATE TABLE stickynotes (
   id VARCHAR(255) PRIMARY KEY,
   user_email VARCHAR(255),
   title VARCHAR(30),
   progress INT,
   date VARCHAR(300)
+);
+
+CREATE TABLE users (
+  email VARCHAR(255) PRIMARY KEY,
+  hashed_password VARCHAR(255)
 );
