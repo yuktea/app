d5353f3c826d55d18199a2d5cda606459735733f
yuktea <73362292+yuktea@users.noreply.github.com>
2023-09-04 13:33:34 +0530

    add POST request method to create new note

--- my-app/server/server.js
+++ my-app/server/server.js
@@ -8,3 +8,15 @@ app.get('/todos/:userEmail', async (req, res) => {
     console.error(err)
   }
 })
+// create a new todo
+app.post('/todos', async(req, res) => {
+  const { user_email, title, progress, date } = req.body
+  const id = uuidv4()
+  try {
+    const newToDo = await pool.query(`INSERT INTO todos(id, user_email, title, progress, date) VALUES($1, $2, $3, $4, $5)`,
+      [id, user_email, title, progress, date])
+    res.json(newToDo)
+  } catch (err) {
+    console.error(err)
+  }
+})
