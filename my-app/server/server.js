c27544b865945d24cc6e950b50e569755a237910
yuktea <73362292+yuktea@users.noreply.github.com>
2023-09-05 17:36:40 +0530

    implement `sign up` for new users

--- my-app/server/server.js
+++ my-app/server/server.js
@@ -26,6 +26,18 @@ app.post('/signup', async (req, res) => {
   const salt = bcrypt.genSaltSync(10)
   const hashedPassword = bcrypt.hashSync(password, salt)
 
+  try {
+    const signUp = await pool.query(`INSERT INTO users (email, hashed_password) VALUES($1, $2)`,
+      [email, hashedPassword])
+  
+    const token = jwt.sign({ email }, 'secret', { expiresIn: '1hr' })
+    
+    res.json({ email, token })
+  } catch (err) {
+    console.error(err)
+    if (err) {
+      res.json({ detail: err.detail})
+    }
   }
 })
 
